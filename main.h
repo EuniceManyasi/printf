67fb20352290ad3f59fb37b1096141f2e0019bb4
D3vinah <devinahmanyasi@gmail.com>
2023-05-31 11:59:52 -0700

    verified main.h

--- main.h
+++ main.h
@@ -20,42 +20,42 @@
 #define CONVERT_UNSIGNED   2
 
 /**
-  * struct parameters - parameters struct
-  *
-  * @unsign: flag if unsigned value
-  *
-  * @plus_flag: on if plus_flag specified
-  * @space_flag: on if hashtag_flag specified
-  * @hash_flag: on if _flag specified
-  * @zero_flag: on if _flag specified
-  * @minus_flag: on if _flag specified
-  *
-  * @width: field width specified
-  * @precision: field precision specified
-  *
-  *@h_modifier: on if h_modifier specified
-  *@l_modifier: on if l_modifier specified
-  *
-  */
+* struct parameters - parameters struct
+*
+* @unsign: flag if unsigned value
+*
+* @plus_flag: on if plus_flag specified
+* @space_flag: on if hashtag_flag specified
+* @hash_flag: on if _flag specified
+* @zero_flag: on if _flag specified
+* @minus_flag: on if _flag specified
+*
+* @width: field width specified
+* @precision: field precision specified
+*
+*@h_modifier: on if h_modifier specified
+*@l_modifier: on if l_modifier specified
+*
+*/
 
 typedef struct parameters
 {
-   unsigned int unsign          : 1;
-   unsigned int plus_flag       : 1;
-   unsigned int space_flag      : 1;
-   unsigned int hashtag_flag    : 1;
-   unsigned int zero_flag       : 1;
-   unsigned int minus_flag      : 1;
+unsigned int unsign          : 1;
+unsigned int plus_flag       : 1;
+unsigned int space_flag      : 1;
+unsigned int hashtag_flag    : 1;
+unsigned int zero_flag       : 1;
+unsigned int minus_flag      : 1;
 
-   unsigned int width;
-   unisigned int precision;
-   
+unsigned int width;
+unisigned int precision;
+
 unsigned int h_modifier        : 1;
 unsigned int l_modifier        : 1;
 } params_t;
 /**
 * struct specifier - Struct token
-* 
+*
 * @specifier: format token
 * @f: The function associated
 */
@@ -74,15 +74,15 @@ int print_string(va_list ap, param_t *params);
 int print_percent(va_list ap, param_t *params);
 int print_S(va_list ap, param_t *params);
 /* number.c module */
-char *convert(long int num, int base, int flags,params_t *params);
+char *convert(long int num, int base, int flags, params_t *params);
 int print_unsigned(va_list ap, param_t *params);
 int print_address(va_list ap, param_t *params);
 /* specifier.c module */
 int (*get_specifier(char *s))(va_list ap, params_t *params);
-int get_print_func(char *s,va_list ap, params_t *params);
+int get_print_func(char *s, va_list ap, params_t *params);
 int get_flag(char *s, params_t *params);
-int get_modifier(char *s,params_t *params);
-int *get_width(char *s,params_t *params, va_list ap);
+int get_modifier(char *s, params_t *params);
+int *get_width(char *s, params_t *params, va_list ap);
 /* convert_number.c module */
 int print _hex(va_list ap, params_t *params);
 int print _HEX(va_list ap, params_t *params);
@@ -99,9 +99,9 @@ int print_number(char *str, params_t *params);
 int print_number_right_shift(char *str, params_t *params);
 int print_number_left_shift(char *str, params_t *params);
 /* params.c module */
-void init_params (params_t *params, va_list ap);
+void init_params(params_t *params, va_list ap);
 /* string_fields.c module */
 char *get_precision(char *p, params_t *params, va_list ap)
 /* _printf.c module */
 int _printf(const char *format, ...);
-#endif; 
+#endif
